74dc27eae94c290454b0e4991a4ea228733c63b7
Khaled Hazime <hazimekhaled01@gmail.com>
2019-10-14 19:32:26 -0300

    publishing tf, not tested yet

--- src/butia_world/plugins/viewer_reader.py
+++ src/butia_world/plugins/viewer_reader.py
@@ -1,14 +1,28 @@
 import rospy
+import tf
+from geometry_msgs.msg import Pose
+
 from .world_plugin import WorldPlugin
 
-class ViewerReaderPlugin(WorldPlugin):
+class ViewerReaderPlugin(WorldPlugin): 
 
   def run(self):
-    #br = tf.TransformBroadcaster()
-    pos_keys = self.r.keys('*/position')
-    ori_keys = self.r.keys('*/orientation')
-    print(keys)
-    #while True:
-    #  time.sleep(1)
-    #  message.data = int(self.r.get('heartbeat'))
-    #  publisher.publish(message)
+    br = tf.TransformBroadcaster()
+    rate = rospy.Rate(30)
+    while not rospy.is_shutdown():
+      pose_keys = self.r.keys('*/pose')
+      pose = Pose()
+      for key in pose_keys:
+        link = key.split('/')[-2]
+        db_pose = self.r.hvals(key)
+        pose.position.x = float(db_pose[0])
+        pose.position.y = float(db_pose[1])
+        pose.position.z = float(db_pose[2])
+        pose.orientation.x = float(db_pose[3])
+        pose.orientation.y = float(db_pose[4])
+        pose.orientation.z = float(db_pose[5])
+        pose.orientation.w = float(db_pose[6])
+        #br.sendTransform(pose.position, pose.orientation, rospy.Time.now(), link, "map")
+
+        print(db_pose)
+      rate.sleep()
