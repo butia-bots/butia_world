4fc7cc6970cd7cb83246ace06665dee69a457fce
Khaled Hazime <hazimekhaled01@gmail.com>
2019-10-14 18:22:54 -0300

    using hash to define pose

--- src/butia_world/plugins/map_writer.py
+++ src/butia_world/plugins/map_writer.py
@@ -5,12 +5,10 @@ from .world_plugin import WorldPlugin
 class MapWriterPlugin(WorldPlugin):
 
   def run(self):
-    points = rospy.get_param('/butia_world/map/points')
+    poses = rospy.get_param('/butia_world/map/targets')
     with self.r.pipeline() as pipe:
-      for p_id, point in points.items():
-        for v_id, value in point.items():
-          key = p_id + '/' + v_id
-          pipe.delete(key)
-          pipe.rpush(key, *value)
+      for p_id, pose in poses.items():
+        print(pose)
+        key = 'target/' + p_id + '/' + 'pose'
+        pipe.hmset(key, pose)
       pipe.execute()
-    #self.r.bgsave()
