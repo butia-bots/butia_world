1dca5a94278832579a87399637a7b88dbdc453c5
Khaled Hazime <hazimekhaled01@gmail.com>
2019-10-15 10:19:24 -0300

    publishing tf of poses in database.

--- src/butia_world/plugins/viewer_reader.py
+++ src/butia_world/plugins/viewer_reader.py
@@ -14,15 +14,18 @@ class ViewerReaderPlugin(WorldPlugin):
       pose = Pose()
       for key in pose_keys:
         link = key.split('/')[-2]
-        db_pose = self.r.hvals(key)
-        pose.position.x = float(db_pose[0])
-        pose.position.y = float(db_pose[1])
-        pose.position.z = float(db_pose[2])
-        pose.orientation.x = float(db_pose[3])
-        pose.orientation.y = float(db_pose[4])
-        pose.orientation.z = float(db_pose[5])
-        pose.orientation.w = float(db_pose[6])
-        #br.sendTransform(pose.position, pose.orientation, rospy.Time.now(), link, "map")
+        db_pose = self.r.hgetall(key)
+        pose.position.x = float(db_pose['px'])
+        pose.position.y = float(db_pose['py'])
+        pose.position.z = float(db_pose['pz'])
+        pose.orientation.x = float(db_pose['ox'])
+        pose.orientation.y = float(db_pose['oy'])
+        pose.orientation.z = float(db_pose['oz'])
+        pose.orientation.w = float(db_pose['ow'])
 
-        print(db_pose)
+        p = pose.position
+        o = pose.orientation
+        br.sendTransform((p.x, p.y, p.z), (o.x, o.y, o.z, o.w), rospy.Time.now(), link, "map")
+
+        #print(db_pose)
       rate.sleep()
