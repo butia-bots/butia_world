9301ca078149b6423864b658cbad63813add9ed1
Igor Maurell <--global>
2019-10-18 21:31:09 -0300

    plugin to read poses

--- src/butia_world/plugins/pose_reader.py
+++ src/butia_world/plugins/pose_reader.py
@@ -1,14 +1,31 @@
 import time
 
 import rospy
+from butia_world_msgs.msg import DBPose
+from butia_world_msgs.srv import GetPoses, GetPosesResponse
 from .world_plugin import WorldPlugin
 
 class PoseReaderPlugin(WorldPlugin):
 
+  def callback(self, req):
+    query = req.query
+    keys = self.r.keys(query)
+    keys = list(filter(lambda x: '/pose' in x, keys))
+    db_poses = []
+    for key in keys:
+      pose = DBPose()
+      pose.label = key.replace('/pose', '')
+      db_pose = self.r.hgetall(key)
+      pose.pose.position.x = float(db_pose['px'])
+      pose.pose.position.y = float(db_pose['py'])
+      pose.pose.position.z = float(db_pose['pz'])
+      pose.pose.orientation.x = float(db_pose['ox'])
+      pose.pose.orientation.y = float(db_pose['oy'])
+      pose.pose.orientation.z = float(db_pose['oz'])
+      pose.pose.orientation.w = float(db_pose['ow'])
+      db_poses.append(pose)
+    return GetPosesResponse(db_poses)
+
   def run(self):
-    publisher = rospy.Publisher('world/heartbeat', Int32)
-    message = Int32()
-    while True:
-      time.sleep(1)
-      message.data = int(self.r.get('heartbeat'))
-      publisher.publish(message)
+    pose_server = rospy.Service('/butia_world/get_poses', GetPoses, self.callback)
+    rospy.spin()
