d3b12bd74ff1578afe0a410c6d1519d7464f5050
Igor Maurell <--global>
2019-10-18 21:31:09 -0300

    plugin to read poses

--- src/butia_world/plugins/__init__.py
+++ src/butia_world/plugins/__init__.py
@@ -4,3 +4,4 @@ from butia_world.plugins.heartbeat_reader import HeartbeatReaderPlugin
 from butia_world.plugins.map_writer import MapWriterPlugin
 from butia_world.plugins.viewer_reader import ViewerReaderPlugin
 from butia_world.plugins.recognition_writer import *
+from butia_world.plugins.pose_reader import PoseReaderPlugin
