a327787e8b7e3a12261e4245907be6ecfec16f5b
Igor Maurell <--global>
2019-10-22 00:42:04 -0300

    new version of plugins

--- src/butia_world/plugins/world_plugin.py
+++ src/butia_world/plugins/world_plugin.py
@@ -6,8 +6,9 @@ import redis
 class WorldPlugin:
 
   def __init__(self):
-    redis_params = rospy.get_param('redis')
+    redis_params = rospy.get_param('/butia_world/redis')
     self.r = redis.Redis(**redis_params)
+    self.fixed_frame = rospy.get_param('/butia_world/fixed_frame')
 
   @abstractmethod
   def run(self):
