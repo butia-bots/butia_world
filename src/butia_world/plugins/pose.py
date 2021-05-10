56302a762d52e131379777d77d4872bd10ec1ccf
crislmfroes <cris.lima.froes@gmail.com>
2021-05-10 23:16:23 +0000

    added 3d object size

--- src/butia_world/plugins/pose.py
+++ src/butia_world/plugins/pose.py
@@ -5,14 +5,16 @@ import tf
 from .world_plugin import WorldPlugin
 from butia_world_msgs.srv import GetKey, GetKeyResponse, GetPose, GetPoseResponse
 
-from geometry_msgs.msg import Pose, PoseStamped
+from geometry_msgs.msg import Pose, PoseStamped, Vector3
 
 from math import sqrt
 
 class PosePlugin(WorldPlugin):
   def readPose(self, key):
     pose = Pose()
+    rospy.loginfo(self.r.keys())
     db_pose = self.r.hgetall(key)
+    rospy.loginfo(db_pose)
     pose.position.x = float(db_pose['px'])
     pose.position.y = float(db_pose['py'])
     pose.position.z = float(db_pose['pz'])
@@ -22,6 +24,18 @@ class PosePlugin(WorldPlugin):
     pose.orientation.w = float(db_pose['ow'])  
     
     return pose
+
+  def readSize(self, key):
+    size = Vector3()
+    db_size = self.r.hgetall(key)
+    try:
+      size.x = float(db_size['sx'])
+      size.y = float(db_size['sy'])
+      size.z = float(db_size['sz'])
+    except Exception:
+      pass
+
+    return size
 
   def setStaticPose(self):
     poses = rospy.get_param('/butia_world/pose/targets')
@@ -35,8 +49,9 @@ class PosePlugin(WorldPlugin):
   def getClosestKey(self, req):
     query = req.query
     keys = self.r.keys(query)
+    rospy.loginfo(keys)
     keys = list(filter(lambda x: '/pose' in x and 'target' not in x, keys))
-
+    rospy.loginfo(keys)
     min_distance = float('inf')
     min_key = None
     for key in keys:
@@ -61,11 +76,14 @@ class PosePlugin(WorldPlugin):
 
   def getPose(self, req):
     key = req.key
+    res = GetPoseResponse()
     pose = self.readPose(key)
-
+    res.pose = pose
     rospy.loginfo(pose)
-
-    return pose
+    size = self.readSize(key)
+    res.size = size
+    rospy.loginfo(size)
+    return res
 
   def run(self):
     self.setStaticPose()
